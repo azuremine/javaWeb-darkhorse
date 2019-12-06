e60daad3e252e71b7cae78097e56e99e44c52f90
skrskr死你 <1007299289@qq.com>
2019-12-06 17:55:49 +0800

    first commit

---
+++ src/main/java/cn/itcast/travel/dao/UserDao.java
@@ -0,0 +1,20 @@
+package cn.itcast.travel.dao;
+
+import cn.itcast.travel.domain.User;
+
+public interface UserDao {
+
+    /**
+     * 根据用户名查询用户信息
+     * @param username
+     * @return
+     */
+    public User findByUsername(String username);
+
+    /**
+     * 用户保存
+     * @param user
+     */
+    public void save(User user);
+
+}
---
+++ src/main/java/cn/itcast/travel/service/UserService.java
@@ -0,0 +1,13 @@
+package cn.itcast.travel.service;
+
+import cn.itcast.travel.domain.User;
+
+public interface UserService {
+    /**
+     * 注册用户
+     *
+     * @param user
+     * @return
+     */
+    boolean regist(User user);
+}
