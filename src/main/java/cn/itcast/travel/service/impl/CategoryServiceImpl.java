a63262ef9c85dc72454bc82ee53323b97f31a138
skrskr死你 <1007299289@qq.com>
2019-12-09 21:15:45 +0800

    导航条目链接携带code信息

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
 import cn.itcast.travel.util.JedisUtil;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +22,12 @@ public class CategoryServiceImpl implements CategoryService {
         //返回是list
         List<Category> list = null;
         //使用sortedSet排序查询
-        Set<String> categorys = jedis.zrange("category",0,-1);
+//        Set<String> categorys = jedis.zrange("category",0,-1);
+        //查询sortedSet中的分数（cid)和值（cname)
+        Set<Tuple> categorys = jedis.zrangeWithScores("category", 0, -1);
         if(categorys == null || categorys.size() == 0){
             //说明第一次查询，缓存中没由数据
             list = categoryDao.findAll();
-            System.out.println(list.toString());
             //存储数据
             for (int i = 0; i < list.size(); i++) {
                 jedis.zadd("category",list.get(i).getCid(),list.get(i).getCname());
@@ -33,13 +35,13 @@ public class CategoryServiceImpl implements CategoryService {
         }else {
             list = new ArrayList<Category>();
             //如果不为空，将set的数据存入list
-            for (String name : categorys) {
+            for (Tuple tuple : categorys) {
                 Category category = new Category();
-                category.setCname(name);
+                category.setCid((int) tuple.getScore());
+                category.setCname(tuple.getElement());
                 list.add(category);
             }
         }
         return list;
     }
-
 }
